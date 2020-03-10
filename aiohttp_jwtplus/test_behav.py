24818b1b82ace0bd1dd51bc1a8590d80ffbc42fe
GoodManWEN <44322449+GoodManWEN@users.noreply.github.com>
2020-03-11 01:28:59 +0800

    Update test_behav.py

--- aiohttp_jwtplus/test_behav.py
+++ aiohttp_jwtplus/test_behav.py
@@ -1,28 +1,15 @@
 def test_behav():   
     '''
-    五类测试页面
-    
-    1、@routes.get('/index.html')
-    spa主页面
-    
-    2、@routes.get('/login.html')
-    spa登录页面，独立
+    # markdown
+    ## Behaviors of routing under different authorization
+    path | remarks | authorized destination |  unauthorized destination
+    -|-|-|-
+    /index.html | Entry of main functional SPA | /index.html | /login.html
+    /login.html | Entry of login page. Independent SPA | /index.html | /login.html
+    /login_api  | Login api , one in jwt whitelist. | /login_api | /login_api
+    /setattr_api | One of protected apis. | /setattr_api | 403 or 401
+    /404        | Undefined page | /index.html | /login.html
 
-    3、@routes.get('/login')
-    登录请求api，处于白名单中，会执行jwt验证，但不会影响路由行为
-    
-    4、@routes.get('/setattr')
-    受保护的api
-
-    5、@routes.get('/404')
-    未定义的页面
-
-    其应表现出的路由结果：
-        logined         unauthorized
-    1   index.html      login.html
-    2   index.html      login.html
-    3   /login正常逻辑   /login正常逻辑
-    4   api正常逻辑      401
-    5   index.html      login.html   
+    */\* Status code 404 handled in SPA \*/*
     '''
     pass
